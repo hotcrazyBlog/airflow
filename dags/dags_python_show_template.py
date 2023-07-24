d1db397a98ba0729f409083ce64e6da90670ed36
hotcrazyBlog <hotcrazycristal@gmail.com>
2023-07-24 14:08:57 +0900

    op_kwargs 테스트용 dag 업로드

--- dags/dags_python_show_template.py
+++ dags/dags_python_show_template.py
@@ -6,8 +6,8 @@ from airflow.decorators import task
 with DAG(
     dag_id="dags_python_show_template",
     schedule="30 9 * * *",
-    start_date=pendulum.datetime(2023, 3, 10, tz="Asia/Seoul"),
-    catchup=False
+    start_date=pendulum.datetime(2023, 7, 10, tz="Asia/Seoul"),
+    catchup=True
 ) as dag:
 
     @task(task_id='python_task')
